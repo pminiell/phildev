75a8fb4c7f191e42cf0838bdc47f97741ac29962
Philip <minielly5@hotmail.com>
2021-02-20 15:05:08 -0500

    projects page is less terrible... still terrbile ... but LESS

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,19 +8,19 @@ const Projects = () => {
 
   return (
     <>
-    
-      <section className="bg-gradient-to-b from-blue-600 to-gray-600 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 lg:h-screen lg:pt-32">
-        
+      <section className="bg-gradient-to-b from-blue-600 to-gray-600 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 h-screen lg:pt-32">
         {items.map((item) => {
-          const { id, path, desc } = item;
+          const { id, path, desc, link } = item;
           return (
             <div className="flex flex-wrap justify-center">
               <div key={id} className="w-124 px-4">
-                <img
-                  src={path}
-                  alt={desc}
-                  className="shadow rounded max-w-full h-auto align-middle border-none"
-                />
+                <a href={link} rel="noreferrer">
+                  <img
+                    src={path}
+                    alt={desc}
+                    className="shadow rounded max-w-full h-auto align-middle border-none opacity-80 hover:opacity-100"
+                  />
+                </a>
                 <small className="block text-center text-white font-semibold text-lg">
                   {desc}
                 </small>
--- src/data.js
+++ src/data.js
@@ -1,19 +1,24 @@
 const data = [
   {
     id: 1,
-    path: "./images/keyboard1.jpg",
-    desc: "This is a keyboard, imagine if it was my first project.",
+    path: "./images/profile-cropped.jpg",
+    desc:
+      "I've spent a lot of time learning the fundamentals of web devlopment to get to the point where building a full-stack MERN app doesn't seem so daunting. I'm excited to continue to create. Keep an eye out to see what else I build as I continuie to learn as much as I can about modern day web applications",
   },
   {
     id: 2,
-    path: "./images/keyobard2.jpg",
-    desc: "This is a different keyboard, imagine if it was my second project.",
+    path: "./images/github.png",
+    desc:
+      "This is my GitHub. I'm trying to use git version control consistently in my workflow. I try to push something everyday.",
+    link: "https://github.com/pminiell",
   },
   {
     id: 3,
-    path: "./images/keyboard3.jpg",
-    desc: "This is a third keyboard, imagine if it was my third project.",
-  }
+    path: "./images/cnmap.png",
+    desc:
+      "This is a full stack MERN app that I built. There's a lot of logic in this project in regards to Authentication and Authorization that was tricky to implement. It also connects to the Google Maps API to parse and display addresses. Very proud of the work I did on this project and look forward to using these technologies more. P.S. File upload works , but because of the way heroku works the images are unfortunately not persistent after site inactiviy. ",
+    link: "https://tag-that-place.herokuapp.com/",
+  },
 ];
 
 export default data;
